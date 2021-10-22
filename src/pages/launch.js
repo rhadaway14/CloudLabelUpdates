34ae5271f6c941a083ed4063d1fd9f9b35b50b4e
Robert Hadaway <rhadaway14@gmail.com>
2021-10-21 22:22:11 -0600

    components directories made, Launch page started, added routes to app.js

---
+++ src/pages/launch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Launch() {
+    return(
+        <>
+            <h1 style={{ marginLeft: '10vw'}}>Create</h1>
+            <h1 style={{ marginLeft: '20vw'}}>Update</h1>
+            <h1 style={{ marginLeft: '5vw'}}>Commit</h1>
+        </>
+    )
+}
